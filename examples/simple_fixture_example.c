884da59e71f63ad3634c9c4a03b570b9ece465f8
PabloMansanet <pablo.mansanet@bluefruit.co.uk>
2015-11-27 11:23:10 +0000

    alternate syntax for clearer examples

--- examples/simple_fixture_example.c
+++ examples/simple_fixture_example.c
@@ -1,11 +1,13 @@
 #include "metal_test.h"
 
-METAL_FIXTURE_DEFINE {
+METAL_FIXTURE_DEFINE 
+{
    unsigned int example_integer;
    float example_float;
 };
 
-METAL_SETUP {
+METAL_SETUP 
+{
    METAL_FIXTURE.example_integer = 7;
    METAL_FIXTURE.example_float = 5.5;
 };
@@ -13,8 +15,9 @@ METAL_SETUP {
 METAL_TEARDOWN {};
 
 METAL_SUITE_BEGIN
-
-   METAL_TEST(example_passing_test_with_fixture) {
+{
+   METAL_TEST(example_passing_test_with_fixture) 
+   {
       METAL_ASSERT(METAL_FIXTURE.example_integer);
 
       int expected_value = 7;
@@ -22,14 +25,16 @@ METAL_SUITE_BEGIN
       METAL_FIXTURE.example_integer = 0; //Irrelevant, since the fixture will be recreated between tests.
    };
 
-   METAL_TEST(example_float_test) {
+   METAL_TEST(example_float_test) 
+   {
       float expected_value = 8.8;
       METAL_ASSERT_EQ(expected_value, METAL_FIXTURE.example_float);
    }
    
-   METAL_TEST(example_failing_test_with_fixture) {
+   METAL_TEST(example_failing_test_with_fixture) 
+   {
       int expected_value = 0;
       METAL_ASSERT_EQ(expected_value, METAL_FIXTURE.example_integer);
    }
-
+} 
 METAL_SUITE_END
--- examples/simple_fixtureless_example.c
+++ examples/simple_fixtureless_example.c
@@ -5,20 +5,23 @@ METAL_SETUP {};
 METAL_TEARDOWN {};
 
 METAL_SUITE_BEGIN
-
-   METAL_TEST(example_passing_test){
+{
+   METAL_TEST(example_passing_test)
+   {
       int test_value = 1;
       METAL_ASSERT(test_value);
    }
 
-   METAL_TEST(example_failing_test) {
+   METAL_TEST(example_failing_test) 
+   {
       int test_value = 0;
       METAL_ASSERT(test_value);
       METAL_ASSERT(test_value);
    }
 
-   METAL_TEST(example_second_failing_test) {
+   METAL_TEST(example_second_failing_test) 
+   {
       METAL_ASSERT_EQ(5,0);
    }
-
-METAL_SUITE_END
+}
+METAL_SUITE_END 
