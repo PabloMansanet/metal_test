fb21449c69ec45538c4c296511ae60be2efe0431
PabloMansanet <pablo.mansanet@bluefruit.co.uk>
2015-12-01 15:44:39 +0000

    Fixed union initialization bug

--- include/implementation/type_punning.h
+++ include/implementation/type_punning.h
@@ -3,10 +3,13 @@
 
 //TODO: Check endianness 
 #define METAL_FILL_UNION(union_name, lvalue) { \
-   union_name.ll = 0; \
    char* lvalue_byte_handle = (char*)&lvalue; \
    char* union_byte_handle = (char*)&union_name; \
    int i; \
+     for (i = 0; i < sizeof(metal_punning_union); i++) \
+     { \
+        union_byte_handle[i] = 0x00; \
+     } \
      for (i = 0; i < sizeof(lvalue); i++) \
      { \
         union_byte_handle[i] = lvalue_byte_handle[i]; \
